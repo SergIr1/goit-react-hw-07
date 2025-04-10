af84d9a0485d45cbe52ac5cdc68645b11792fbc4
Serhii Karskyi <serhiipraktic7@gmail.com>
2025-04-10 21:40:50 +0100

    fix bag

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,40 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsSliceReducer from './contactsSlice';
 import filtersSliceReducer from './filtersSlice';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-
-const contactsPersistConfig = {
-  key: 'user_contacts',
-  storage,
-  whitelist: ['items'],
-};
-
-const persistedContactsReducer = persistReducer(
-  contactsPersistConfig,
-  contactsSliceReducer
-);
 
 export const store = configureStore({
   reducer: {
-    contacts: persistedContactsReducer,
+    contacts: contactsSliceReducer,
     filters: filtersSliceReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
